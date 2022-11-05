8f11ea72b496eaf758dad5ce68047608a7d2d318
fahrizyxx <111411253+fahrizyxx@users.noreply.github.com>
2022-11-05 18:17:11 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -9,7 +9,7 @@ const catalog = generateWAMessageFromContent(m.chat, proto.Message.fromObject({
 "product": {
 "productImage": messa.imageMessage, 
 "productId": "5489299044451325",
-"title": `Jasa Sewa Yushino Md`,
+"title": `Jasa Sewa Fzec Md`,
 "description": `HALO BANG`,
 "currencyCode": "IDR",
 "bodyText": wm,
