da465bfae9460704622e9f17c3bbdb740278712a
fahrizyxx <111411253+fahrizyxx@users.noreply.github.com>
2022-11-21 18:03:24 +0700

    Update downloader-tiktokmp4.js

--- plugins/downloader-tiktokmp4.js
+++ plugins/downloader-tiktokmp4.js
@@ -13,7 +13,7 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
 ▥ ━┉┄┄┈┈ ▢
 ${res.author}
 ◈ ━┉┈┄┈┈ ►`
-    conn.sendButtonVid(m.chat, res.video, result, '_© Created by fahri_', `Audio`, `.gettt ${args[0]}`, m)
+    conn.sendButtonVid(m.chat, res.video, result, '_© 𝗖𝗿𝗲𝗮𝘁𝗲𝗱 by fahri_', `Audio`, `.gettt ${args[0]}`, m)
 }
 handler.help = ['tiktok'].map(v => v + ' <url>')
 handler.tags = ['downloader']
